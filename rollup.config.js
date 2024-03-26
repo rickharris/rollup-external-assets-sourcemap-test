48559c7e5d612b21d68c263a236517d6e79c281f
Rick Harris <rickharris@users.noreply.github.com>
2024-03-26 14:50:11 -0700

    two separate rollup configs to show the difference in sourcemaps with plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "rollup";
 import { externalAssets } from "rollup-plugin-external-assets";
 
-export default defineConfig({
-  input: "./index.js",
-  output: {
-    dir: "dist",
-    sourcemap: true,
-  },
-  plugins: [externalAssets({ include: ["**/*.png"] })],
-});
+export default [
+  defineConfig({
+    input: "./without-assets.js",
+    output: {
+      dir: "dist",
+      sourcemap: true,
+    },
+  }),
+  defineConfig({
+    input: "./with-assets.js",
+    output: {
+      dir: "dist",
+      sourcemap: true,
+    },
+    plugins: [externalAssets({ include: ["**/*.png"] })],
+  }),
+];
